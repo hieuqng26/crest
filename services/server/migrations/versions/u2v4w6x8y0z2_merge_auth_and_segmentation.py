c58c1399343681ea9bcfc0b09bab38032a2fbfe7
hieung <reynoldng26@gmail.com>
2026-06-30 11:19:18 +0800

    fix(migrations): merge auth_rebuild and segmentation heads into single head

---
+++ services/server/migrations/versions/u2v4w6x8y0z2_merge_auth_and_segmentation.py
@@ -0,0 +1,23 @@
+"""merge auth_rebuild and segmentation heads
+
+Revision ID: u2v4w6x8y0z2
+Revises: 881c8b52ff57, t0u2v4w6x8y0
+Create Date: 2026-06-30
+
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "u2v4w6x8y0z2"
+down_revision = ("881c8b52ff57", "t0u2v4w6x8y0")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
